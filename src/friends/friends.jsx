21cc96928f976b8c9e9089b87b4210e5d135ebcd
Ben Brittain <38736115+thelastbrittain@users.noreply.github.com>
2024-11-09 17:52:28 -0700

    Finish friends list

--- src/friends/friends.jsx
+++ src/friends/friends.jsx
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function Friends() {
+  const [climbers, setClimbers] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const climberList = localStorage.getItem('climbers');
+    if (climberList) {
+      setClimbers(JSON.parse(climberList));
+    }
+    console.log(climberList);
+  }, []);
+
+  const handleRowClick = () => {
+    navigate("/log");
+  }
+
+  // Demonstrates rendering an array with React
+  const climberRows = [];
+  if (climbers.length) {
+    for (const [i, climber] of climbers.entries()) {
+      climberRows.push(
+        <tr key={i}
+        onClick={handleRowClick} style={{ cursor: 'pointer' }}>
+          <td>{i}</td>
+          <td>{climber.userName}</td>
+          <td>{`${climber.hardestGrade.prefix}.${climber.hardestGrade.suffix}`}</td>
+          <td>{climber.numRoutesClimbed}</td>
+        </tr>
+      );
+    }
+  } else {
+    climberRows.push(
+      <tr key='0'>
+        <td colSpan='4'>Be the first to log your climbs!</td>
+      </tr>
+    );
+  }
+
   return (
     <main>
       {/* <!-- Columns:  Name, hardest route, number of routes climbed --> */}
@@ -15,26 +53,7 @@ export function Friends() {
                   <th>Routes logged</th>
               </tr>
           </thead>
-          <tbody className="table-dark">
-              <tr>
-                  <td>1</td>
-                  <td><a className="friends-a" href="log.html">Joe</a></td>
-                  <td>10c</td>
-                  <td>30</td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td><a className="friends-a" href="log.html">Adam Ondra</a></td>
-                  <td>15d</td>
-                  <td>4987</td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td><a className="friends-a" href="log.html">Tommy Caldwell</a></td>
-                  <td>15a</td>
-                  <td>4988</td>
-                </tr>
-          </tbody>
+          <tbody className="table-dark">{climberRows}</tbody>
       </table>
       <h3>This will be database data</h3>
     </main>
